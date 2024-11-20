43a93b2e3569e387f0c92d3ef28c167930c4eccd
Karuiru Jackson <jkjackaruiru641@gmail.com>
2024-11-20 15:02:20 +0300

    Make the footer section responsive

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -6,12 +6,12 @@ import Instagram from '../assets/Instagram.svg';
 function Footer() {
   return (
     <section className='relative top-28'>
-        <footer className="flex flex-col items-center justify-center px-4 gap-8">
-            <div className="flex gap-6">
-                <Image src={Instagram} alt='Instagram-Logo' />
-                <Image src={XLogo} alt='X-Logo' />
-            </div>
-            <span className=" flex items-center justify-center leading-6 font-normal text-[#9CA3AF] text-base">© 2024 Along With, Inc. All rights reserved.</span>
+        <footer className="flex flex-col md:flex-row md:justify-between items-center justify-center md:px-8 md:py-12 px-4 gap-8">
+          <span className=" flex items-center justify-center leading-6 font-normal text-[#9CA3AF] text-base">© 2024 Along With, Inc. All rights reserved.</span>
+          <div className="flex gap-6">
+            <Image src={Instagram} alt='Instagram-Logo' />
+            <Image src={XLogo} alt='X-Logo' />
+          </div>
         </footer>
     </section>
   )
